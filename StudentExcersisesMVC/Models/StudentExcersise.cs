40d74f25fffc6a132d7d1bb8800379f911150c94
Case Scally <casescally@gmail.com>
2020-04-08 14:56:15 -0500

    first commit

---
+++ StudentExcersisesMVC/Models/StudentExcersise.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExcersisesMVC.Models
+{
+    public class StudentExcersise
+    {
+        public int ExcersiseId { get; set; }
+        public int StudentId { get; set; }
+        public List<Excersise> AssignedExcersise { get; set; } = new List <Excersise>();
+    }
+}
